da679e875e6160a9b1858ae10a1879ed5f06642a
grGrimales <gredygrimales@gmail.com>
2021-03-26 14:57:24 -0500

    Se añadio header y navbar al apartado publico

--- src/app/@public/pages/public.module.ts
+++ src/app/@public/pages/public.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
-
+import { HeaderComponent } from '../core/components/header/header.component';
+import { NavbarComponent } from '../core/components/navbar/navbar.component';
+import { FooterComponent } from '../core/components/footer/footer.component';
 
 @NgModule({
-  declarations: [PublicComponent],
+  declarations: [PublicComponent, HeaderComponent, NavbarComponent, FooterComponent],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    
   ]
 })
 export class PublicModule { }
