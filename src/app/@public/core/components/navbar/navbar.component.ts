8637dda9722fd02732101b69fc4ae1561d93c251
grGrimales <gredygrimales@gmail.com>
2021-03-31 19:57:18 -0500

    s

--- src/app/@public/core/components/navbar/navbar.component.ts
+++ src/app/@public/core/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
+import { AuthResponse } from 'src/app/core/interfaces/login.interface';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  
-  constructor() {
+
+  access = false;
+  role: string;
+  userLabel = '';
+  nombre: string;
+
+
+  constructor(private auth: AuthService) {
+    this.auth.accessVar$.subscribe((result) => {
+      console.log(result);
+      this.userLabel = `${result.usuario!.nombre}` ;
+    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+     
+  }
+
+  logout() {
+    this.auth.logout();
+  }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { AuthResponse, Usuario } from '../interfaces/login.interface';
@@ -15,6 +15,9 @@ export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
   private _usuario!: Usuario;
+  
+  accessVar = new Subject<any>();
+  accessVar$ = this.accessVar.asObservable();
 
   get usuario() {
     return { ...this._usuario };
@@ -23,16 +26,20 @@ export class AuthService {
   constructor( private http: HttpClient ) { }
 
   //Registro de usuario
-  registro( nombre: string, apellido: string, email: string, password: string, role: string ="USER_ROLE" ) {
+  registro( nombre: string, apellido: string, email: string, password: string, rol: string ="USER_ROLE" ) {
 
     const url  = `${ this.baseUrl }usuarios`;
-    const body = { email, password, nombre, apellido, role };
+    const body = { email, password, nombre, apellido, rol };
+
+
+    console.log(body);
 
     return this.http.post<AuthResponse>( url, body )
       .pipe(
         tap( ({ ok, token }) => {
           if ( ok ) {
             localStorage.setItem('token', token! );
+            
           }
         }),
         map( resp => resp.ok ),
@@ -49,16 +56,26 @@ export class AuthService {
     const url  = `${ this.baseUrl }auth/login`;
     const body = { email, password };
 
-    return this.http.post<AuthResponse>( url, body )
+    return this.http.post<any>( url, body )
       .pipe(
         tap( resp => {
+          console.log(resp);
           if ( resp.ok ) {
             localStorage.setItem('token', resp.token! );
+            localStorage.setItem('usuario', resp.usuario.nombre! );
           }
         }),
         map( resp => resp.ok ),
         catchError( err => of(err.error.msg) )
       );
   }
+
+
+  
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
 
 }
