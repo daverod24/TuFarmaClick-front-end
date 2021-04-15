4d4438ac3e0d6393096dbd0c5c91dd621d05b181
grGrimales <gredygrimales@gmail.com>
2021-04-15 17:44:35 -0500

    Funcionalida de agregar productos y total

--- src/app/@public/pages/orders/orders.component.ts
+++ src/app/@public/pages/orders/orders.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
+  productosAgregados: number = 0;
+  total: number = 0;
+  precio: number = 25;
+
+  get getProductosAgregados() {
+    return this.productosAgregados;
+  }
+
+
+  sumaProductos( valor: number ){
+    this.productosAgregados = this.productosAgregados + valor;
+    this.total = this.productosAgregados * this.precio;
+  }
+
+  restaProductos( valor: number){
+    this.productosAgregados = this.productosAgregados - valor
+    this.total = this.productosAgregados * this.precio;
+
+    if(this.productosAgregados <= 0){
+     this.productosAgregados = 0
+    return this.total = 0
+     
+    }
+   
+
+  }
+
+  
+
   constructor() { }
 
   ngOnInit(): void {
